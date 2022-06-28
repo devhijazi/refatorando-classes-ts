054c7613dab3883fd79adb1dfce0d323bf13495b
devhijazi <heilhijzi@outlook.com>
2022-06-28 13:10:24 -0300

    changing files from jsx to tsx

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -13,8 +13,8 @@ export const Container = styled.div`
     text-align: center;
 
     ${props =>
-    !props.available &&
-    css`
+      !props.available &&
+      css`
         opacity: 0.3;
       `};
 
